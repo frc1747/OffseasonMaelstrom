028715df61e7c80296765f24ba500b31e7d70d51
GoldLemur <goldlemur82303@gmail.com>
2025-02-24 20:10:41 -0500

    RELEASE THE KRAKEN!!!! Switched elevator from spark to talon, actually saved changes this time

--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -4,14 +4,8 @@
 
 package frc.robot.subsystems;
 
-import com.revrobotics.spark.SparkAbsoluteEncoder;
-import com.revrobotics.spark.SparkBase;
-import com.revrobotics.spark.SparkBase.PersistMode;
-import com.revrobotics.spark.SparkBase.ResetMode;
-import com.revrobotics.spark.SparkClosedLoopController;
-import com.revrobotics.spark.SparkLowLevel.MotorType;
-import com.revrobotics.spark.SparkMax;
-import com.revrobotics.spark.config.SparkMaxConfig;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.NeutralModeValue;
 
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -19,34 +13,15 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class Elevator extends SubsystemBase {
-  private SparkMax elevator;
+  private TalonFX elevator;
   private DigitalInput limitSwitchTop;
   private DigitalInput limitSwitchBottom;
-  private SparkClosedLoopController controller;
-  private SparkAbsoluteEncoder encoder;
 
   public Elevator() {
-    elevator = new SparkMax(Constants.Elevator.ELEVATOR_ID, MotorType.kBrushless);
+    elevator = new TalonFX(Constants.Elevator.ELEVATOR_ID);
+    elevator.setNeutralMode(NeutralModeValue.Brake);
     limitSwitchTop = new DigitalInput(Constants.Elevator.LIMIT_SWITCH_TOP_ID);
     limitSwitchBottom = new DigitalInput(Constants.Elevator.LIMIT_SWITCH_BOTTOM_ID);
-    controller = elevator.getClosedLoopController();
-    encoder = elevator.getAbsoluteEncoder();
-    double p = Constants.Elevator.PID_P;
-    double i = Constants.Elevator.PID_I;
-    double d = Constants.Elevator.PID_D;
-    double f = Constants.Elevator.PID_F;
-
-    SparkMaxConfig config = new SparkMaxConfig();
-    config
-      .closedLoop
-        .pidf(
-          p,
-          i,
-          d,
-          f
-        );
-    
-    elevator.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
   }
   public boolean isAtTop() {
     return  !limitSwitchTop.get();
@@ -59,20 +34,15 @@ public class Elevator extends SubsystemBase {
   }
 
   public void setPosition(double position) {
-    controller.setReference(position, SparkBase.ControlType.kPosition);
+    elevator.setPosition(position);
   }
 
   public double getPosition() {
-    return encoder.getPosition();
-  }
-
-  public double getVelocity() {
-    return encoder.getVelocity();
+    return elevator.getPosition().getValueAsDouble();
   }
 
   @Override
   public void periodic() {
     SmartDashboard.putNumber("Elevator Position", getPosition());
-    SmartDashboard.putNumber("Elevator Velocity", getVelocity());
   }
 }
