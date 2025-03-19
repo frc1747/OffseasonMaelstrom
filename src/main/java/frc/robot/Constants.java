62f887453e15f45bafb20aa9709ab89b41942531
GoldLemur82303 <goldlemur82303@gmail.com>
2025-03-18 20:22:49 -0400

    lowered max angular velocity

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -78,7 +78,7 @@ public class Constants {
     // TODO: Tune these later
     public static final double MAX_SPEED = 4.1;  // Max speed in m/s
     public static final double MAX_ACCEL = 4.1;  // Max acceleration in m/s
-    public static final double maxAngularVelocity = 10.0;  // Rad/s
+    public static final double maxAngularVelocity = 6.0;  // Rad/s
 
     // TODO: Tune these later
     public static final double DRIVE_KP = 0.05;
@@ -156,7 +156,7 @@ public class Constants {
     public static final int INTAKE_ID = 45;
     public static final int CORAL_LIMIT_SWITCH_ID = 6;
     public static final double INTAKE_SPEED = 0.5;
-    public static final double OUTTAKE_SPEED = 0.35;
+    public static final double OUTTAKE_SPEED = 0.8;
   }
   
   public static final class CoralPivot {
